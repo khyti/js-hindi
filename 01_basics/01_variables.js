da67bfaef39ac25884ee4ee745968f1bdc8e6725
khyti <121668295+khyti@users.noreply.github.com>
2025-02-07 08:27:29 +0000

    const,var,let

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+
+
+const account_id=144553
+let accountEmail = "khyati@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+//acount_id=2222 .. not allowed const can't be changed
+
+
+/*
+PREFER NOT TO USE var DUE TO ITS BLOCK SCOPE AND FUNCTIONAL SCOPE..
+*/ 
+accountEmail="kb@gmailcom"
+accountPassword="2121212"
+console.table([account_id,accountEmail,accountPassword,, accountState]);
