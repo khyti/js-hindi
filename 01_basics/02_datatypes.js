9f6e39042b439eb75f32dcddca3d98a499efdd59
khyti <121668295+khyti@users.noreply.github.com>
2025-02-07 08:54:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"user strict"
+
+const { log } = require("util");
+
+//alert(2+2) nodejs has different syantax
+
+
+
+console.log(3 + 3);//code readability should be high
+
+
+let state;
+let num=null
+let number=12345
+let str = "khyati"
+let isLoggedIn = true
+console.log(typeof isLoggedIn);
+console.log(typeof state);//undefined
+console.log(typeof null);//object
